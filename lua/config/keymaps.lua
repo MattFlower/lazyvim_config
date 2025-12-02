31a0d94fe1cba4a434dbd0549ce93b86a813a214
Matthew Flower <mflower@squarespace.com>
2025-12-02 08:54:23 -0500

    Add command to bring up k9s

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,14 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>ch", "<cmd>lua vim.lsp.buf.hover()<cr>", { desc = "Hover Info" })
 vim.keymap.set("n", "<leader>cr", "<cmd>lua vim.lsp.buf.references()<cr>", { desc = "References" })
+
+vim.keymap.set("n", "<leader>k", function()
+  local current_dir = vim.fn.getcwd()
+  Snacks.terminal.toggle({ "k9s" }, {
+    cwd = current_dir,
+    title = "k9s",
+    env = {
+      TERM = "xterm-256color",
+    },
+  })
+end, { desc = "k9s" })
