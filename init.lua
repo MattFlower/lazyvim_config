3d4eed78adccf0f7eed631ec703f6c1b14d27696
Matthew Flower <mflower@squarespace.com>
2025-12-02 08:55:21 -0500

    Add tree-sitter parsing for libsonnet files.  Turn on auto-write for files.  Also, specify all the tree-sitter plugins I want to have installed.

--- init.lua
+++ init.lua
@@ -3,3 +3,11 @@ require("config.lazy")
 
 vim.cmd([[ let $JAVA_HOME = "/Users/mflower/.sdkman/candidates/java/21.0.9-zulu" ]])
 vim.opt.shell = "/bin/zsh"
+
+vim.filetype.add({
+  extension = {
+    libsonnet = "jsonnet",
+  },
+})
+
+vim.cmd([[set autowriteall]])
