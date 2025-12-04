f3ffaf511bf34d246874940a6e56ab1908a1772a
Matthew Flower <mflower@squarespace.com>
2025-12-03 22:26:00 -0500

    Add some comments.  Also disable the copilot lsp server seems how the copilot plugin is disabled anyhow.

--- init.lua
+++ init.lua
@@ -1,15 +1,21 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- Ensure we have a good default JDK
 vim.cmd([[ let $JAVA_HOME = "/Users/mflower/.sdkman/candidates/java/21.0.9-zulu" ]])
+
+-- I like ZSH
 vim.opt.shell = "/bin/zsh"
 
+-- Make sure *.libsonnet files are treated as jsonnet
 vim.filetype.add({
   extension = {
     libsonnet = "jsonnet",
   },
 })
 
+-- Auto save all files when switching buffers or windows
 vim.cmd([[set autowriteall]])
 
-vim.lsp.enable("copilot_ls")
+-- Make sure the copilot lsp server is loaded
+-- vim.lsp.enable("copilot_ls")
