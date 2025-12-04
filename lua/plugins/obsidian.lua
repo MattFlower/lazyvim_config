0a8570426de1ecbdf2a09b0a207b34862bc28b07
Matthew Flower <mflower@squarespace.com>
2025-12-03 22:25:29 -0500

    Switch to snacks in obsidian-nvim.  Also set up some more shortcuts.

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -2,13 +2,24 @@ return {
   {
     "obsidian-nvim/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
+    dependencies = {
+      { "nvim-mini/mini.nvim", version = "*" },
+      { "obsidian-nvim/calendar.nvim" },
+      { "jmbuhr/otter.nvim" },
+    },
     ft = "markdown",
     opts = {
+      ui = {
+        enable = false,
+      },
       daily_notes = {
         folder = "journal",
         date_format = "%Y-%m-%d",
       },
       legacy_commands = false, -- this will be removed in the next major release
+      picker = {
+        name = "snacks",
+      },
       workspaces = {
         {
           name = "Brain",
@@ -17,6 +28,7 @@ return {
       },
     },
     keys = {
+      { "<C-l>", "<cmd>Obsidian toggle_checkbox<cr>", desc = "Toggle Checkbox" },
       { "<leader>o", "", desc = "Obsidian", mode = "n" },
       { "<leader>oa", "<cmd>Obsidian open<cr>", desc = "Open in Obsidian...", mode = "n" },
       { "<leader>od", "<cmd>Obsidian dailies<cr>", desc = "Dailies" },
@@ -24,6 +36,9 @@ return {
       { "<leader>oo", "<cmd>Obsidian quick_switch<cr>", desc = "Open", mode = "n" },
       { "<leader>os", "<cmd>Obsidian search<cr>", desc = "Search notes" },
       { "<leader>ot", "<cmd>Obsidian toc<cr>", desc = "Note TOC" },
+      { "<leader>o-", "<cmd>Obsidian yesterday<cr>", desc = "Yesterday's note" },
+      { "<leader>o0", "<cmd>Obsidian today<cr>", desc = "Today's note" },
+      { "<leader>o1", "<cmd>Obsidian tomorrow<cr>", desc = "Tomorrow's note" },
       {
         "<leader>ox",
         function()
