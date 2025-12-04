5fcb6565988926093b303008e5a8630c56f15234
Matthew Flower <mflower@squarespace.com>
2025-12-03 22:20:34 -0500

    Set up menu items and terminal popup windows for more tui apps.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,7 +4,8 @@
 vim.keymap.set("n", "<leader>ch", "<cmd>lua vim.lsp.buf.hover()<cr>", { desc = "Hover Info" })
 vim.keymap.set("n", "<leader>cr", "<cmd>lua vim.lsp.buf.references()<cr>", { desc = "References" })
 
-vim.keymap.set("n", "<leader>k", function()
+vim.keymap.set("n", "<leader>z", "", { desc = "Apps" })
+vim.keymap.set("n", "<leader>zk", function()
   local current_dir = vim.fn.getcwd()
   Snacks.terminal.toggle({ "k9s" }, {
     cwd = current_dir,
@@ -14,3 +15,31 @@ vim.keymap.set("n", "<leader>k", function()
     },
   })
 end, { desc = "k9s" })
+
+vim.keymap.set("n", "<leader>zs", function()
+  local current_dir = vim.fn.getcwd()
+  Snacks.terminal.toggle({ "spotify_player" }, {
+    title = "Spotify",
+    keys = {
+      h = "hide",
+    },
+    env = {
+      TERM = "xterm-256color",
+    },
+  })
+end, { desc = "Spotify" })
+
+vim.keymap.set("n", "<leader>zd", function()
+  Snacks.terminal.toggle({ "lazydocker" }, {
+    title = "Docker",
+    keys = {
+      h = "hide",
+    },
+  })
+end, { desc = "LazyDocker" })
+
+vim.keymap.set("n", "<leader>zm", function()
+  Snacks.terminal.toggle({ "vi-mongo" }, {
+    title = "vi-mongo",
+  })
+end, { desc = "Mongo (vi-mongo)" })
