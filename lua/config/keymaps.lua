cd5699da22276d79bfc352f35469ad13593719db
Matthew Flower <MattFlower@yahoo.com>
2026-02-16 20:33:52 -0500

    Switch to live-rename so we can more easily edit the name.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>ch", "<cmd>lua vim.lsp.buf.hover()<cr>", { desc = "Hover Info" })
-vim.keymap.set("n", "<leader>cr", "<cmd>lua vim.lsp.buf.references()<cr>", { desc = "References" })
+vim.keymap.set("n", "<leader>cR", "<cmd>lua vim.lsp.buf.references()<cr>", { desc = "References" })
 
 vim.keymap.set("n", "<leader>z", "", { desc = "Apps" })
 vim.keymap.set("n", "<leader>zk", function()
