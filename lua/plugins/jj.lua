f470b84cf5e356367b6214bb5bbe24f88560da20
Matthew Flower <mflower@squarespace.com>
2026-08-25 09:09:06 -0400

    jj plugin setup

---
+++ lua/plugins/jj.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nicolasgb/jj.nvim",
+    version = "*", -- Use latest stable release
+    -- Or from the main branch (uncomment the branch line and comment the version line)
+    -- branch = "main",
+    config = function()
+      require("jj").setup({})
+    end,
+  },
+}
